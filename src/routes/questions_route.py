982470626f8c48df6a57e5d6603c4643950a20b1
sanCrystal <anyanwu.amanzearthur@gmail.com>
2022-07-18 00:32:58 +0100

    dynamic generating of certificates and ipfs uploads completed

--- src/routes/questions_route.py
+++ src/routes/questions_route.py
@@ -1,3 +1,25 @@
 from fastapi import APIRouter
+from src.schema.questions_model import Question
+# from src.controller.question_controls import get_questions, get_question, create_question, update_question, delete_question
 
 router = APIRouter()
+
+#create questions
+@router.post("/questions")
+async def create_question(question: Question):
+    return question
+
+#get questions/ random by category
+@router.get("/questions/{category}")
+async def get_questions(category: str):
+    return category
+
+#update questions
+@router.put("/questions/{question_id}")
+async def update_question(question_id: int, question: Question):
+    return question
+
+#delete questions
+@router.delete("/questions/{question_id}")
+async def delete_question(question_id: int):
+    return question_id
